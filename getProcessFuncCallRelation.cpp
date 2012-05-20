20b2807070d61e602dc50c99915295b640dcf24f
zhichang <microinside@163.com>
2012-05-21 02:35:43 +0800

    ver 2.1

--- getProcessFuncCallRelation.cpp
+++ getProcessFuncCallRelation.cpp
@@ -12,10 +12,6 @@
               BP 含有一位的等莫名其妙的数值
   函数列表   :
               backTrace
-              CallStack.addFunc
-              CallStack.CallStack
-              CallStack.clearStack
-              CallStack.~CallStack
               finish
               funcPackage
               main
@@ -31,175 +27,18 @@
 /*----------------------------------------------*
  * 包含头文件                                   *
  *----------------------------------------------*/
-#include <fstream>
-#include <iomanip>
 #include <iostream>
 #include <string>
-#include "pin.H"
-
 #include <stack>
-#include <sstream>
-#include <ctime>
 
+#include "pin.H"
+#include "CallStack.h"
+#include "CallStack.cpp"
+#include "getProcessFuncCallRelation.h"
 /*----------------------------------------------*
  * 常量定义                                     *
  *----------------------------------------------*/
 const string EXCEPT_SEC = "/lib/";
-const string LOG_NAME = "Log/axiba.log";
-const unsigned long MAX_LINES = 5;
-
-/*------------------------------------------------------------*
- *函数记录类--------------------------------------------------*
- *三个成员: 函数名, 函数起始地址, 本次调用本函数的函数的EBP---*
- *------------------------------------------------------------*/
-class FuncItem
-{
-public:
-    string funcName;
-    ADDRINT funcAddr;
-    ADDRINT upperFuncBP;
-
-    FuncItem(string name, ADDRINT addr, ADDRINT upperBP)
-        :funcName(name), funcAddr(addr), upperFuncBP(upperBP) {}
-};
-/*
- *记录函数调用关系堆栈, 控制相应日志输出类
- *通过比较当前函数主调的EBP, 判断前面函数终止情况
-*/
-class CallStack
-{
-private:
-    std::stack<FuncItem>_funcs;
-    string _outFileName;
-    ofstream _outFile;
-    ofstream  _outTestFile;
-
-    unsigned long _logLines;
-    unsigned long _logFileNum;
-    ostringstream* _pLogStream;
-
-    void out2logFiles();
-
-public:
-    CallStack(string outFileName);
-    ~CallStack();
-    //添加一个函数调用进栈
-    void addFunc(const char* funcName, ADDRINT funcAddr, ADDRINT upperFuncBP);
-    void clearStack();
-
-    //方便调试本程序的两个ofstream引用
-    //标准日志输出文件
-    ofstream& getOutFile()
-    {
-        return _outFile;
-    }
-    //后者是专门为测试的文件
-    ofstream& getTestFile()
-    {
-        return _outTestFile;
-    }
-};
-
-//打开记录文件, 设置输出数据为16进制格式
-CallStack::CallStack(string outFileName = LOG_NAME):_outFileName(outFileName)
-{
-    //open(const char*, std::ios_base::openmode)
-    _outFile.open(_outFileName.c_str());
-    //! 判断打开成功
-    _outFile << hex;
-
-    _outTestFile.open(("test_"+_outFileName).c_str());
-    //! 判断打开成功
-    _outTestFile << hex;
-
-    _pLogStream= new ostringstream();
-    (*_pLogStream) << hex;
-    _logLines = 0;
-    _logFileNum = 0;
-}
-
-//清空函数调用关系栈, 关闭记录文件
-CallStack::~CallStack()
-{
-    clearStack();
-
-    _outFile.close();
-    _outTestFile.close();
-
-    (*_pLogStream) << "/*The end*/";
-    out2logFiles ();
-    delete _pLogStream;
-    _pLogStream = NULL;
-}
-
-//清空函数调用关系栈, 打印相关信息
-void CallStack::clearStack()
-{
-    while(!_funcs.empty())
-    {
-        _outFile << "}//" << _funcs.top().funcName << std::endl;
-        (*_pLogStream) << "}//" << _funcs.top().funcName << std::endl;
-
-        _funcs.pop();
-    }
-}
-
-//添加一个函数调用进栈, 通过比较主调当前函数的函数的EBP控制出栈, 输出相关信息
-void CallStack::addFunc(const char* funcName, ADDRINT funcAddr,
-                        ADDRINT upperFuncBP)
-{
-    if(0 == upperFuncBP)
-    {
-        clearStack();
-        //return;
-    }
-    else
-    {
-        //!clear the BP little than now BP
-        while((!_funcs.empty()) && (0!=_funcs.top().upperFuncBP)
-                && (_funcs.top().upperFuncBP<=upperFuncBP))
-        {
-            _outFile << "}//" << _funcs.top().funcName << std::endl;
-            (*_pLogStream) << "}//" << _funcs.top().funcName << std::endl;
-
-            _funcs.pop();
-        }
-    }
-    //判断是否到达MAX_LINES行, 超出则分摊到新的文件
-    if(MAX_LINES <= _logLines)
-    {
-        out2logFiles ();
-
-        delete _pLogStream;
-        _pLogStream = new ostringstream();
-    }
-
-    time_t nowTime = time(NULL);
-    string nowTimeStr = ctime(&nowTime);
-    //output
-    _outFile << funcName << "{//" << funcAddr << ", " << nowTimeStr
-             << std::endl;
-    (*_pLogStream) << funcName << "{//" << funcAddr << ", " << nowTimeStr
-                   << std::endl;
-    ++ _logLines;
-
-    _funcs.push(FuncItem(funcName, funcAddr, upperFuncBP));
-}
-
-void CallStack::out2logFiles()
-{
-    ostringstream osstream;
-    osstream << _outFileName << "." << _logFileNum;
-
-    ofstream splitFile;
-    splitFile.open(osstream.str().c_str());
-    //输出到上传日志文件, 以--结尾的表示程序未终止
-    splitFile << (*_pLogStream).str();
-    splitFile.close();
-
-    ++ _logFileNum;
-    _logLines = 0;
-}
 
 /*----------------------------------------------*
  * 全局变量                                     *
